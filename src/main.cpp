92e25cdf17bb1113286aff335807706f640fddba
lucky <mazmrini@hotmail.com>
2017-05-15 23:37:02 -0400

    first mesh working

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include "display.h"
 #include "shader.h"
+#include "mesh.h"
 
 using namespace std;
 
@@ -19,9 +20,21 @@ int main() {
 	// extensions are added in constructor
 	Shader shader("./res/basicShader");
 
+	// coordinates are as in a normal graph in (x, y, z) format
+	Vertex vertices[] = { Vertex(glm::vec3(-0.25, -0.25, 0)),
+						  Vertex(glm::vec3(0, 0.25, 0)),
+						  Vertex(glm::vec3(0.25, -0.25, 0))
+						};
+
+	unsigned int nbVertices = sizeof(vertices) / sizeof(vertices[0]);
+	Mesh mesh(vertices, nbVertices);
+
 	while (!display.isClosed()) {
 		display.clear();
+
 		shader.bind();
+		mesh.draw();
+
 		display.update();
 	}
 
